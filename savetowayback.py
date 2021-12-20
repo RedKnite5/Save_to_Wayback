fc4469b1753058e54700330a72b7e2965db1e53e
RedKnite5 <mr.awesome10000@gmail.com>
2021-12-19 23:55:28 -0800

    init with urllist

---
+++ savetowayback.py
@@ -0,0 +1,162 @@
+#!/mnt/c/Users/RedKnite/AppData/Local/Programs/Python/Python38/python.exe
+# save_to_wayback.py
+# last used 1/9/21
+
+import sys
+import time
+from urllib.parse import urljoin
+from urllib3.exceptions import ProtocolError
+import bs4
+import requests
+import savepagenow as save
+
+def ffn_btn(tag):
+	try:
+		assert ["btn"] == tag["class"]
+		assert tag.text == "Next >"
+		return True
+	except (KeyError, AssertionError) as e:
+		return False
+
+def get_ffn(url):
+	page = requests.get(url)
+	soup = bs4.BeautifulSoup(page.text, "html.parser")
+	btns = soup.find_all(ffn_btn)
+	try:
+		assert btns[0]["onclick"] == btns[1]["onclick"]
+	except IndexError:
+		return None
+	assert btns[0]["onclick"].startswith("self.location='")
+
+	return urljoin("https://www.fanfiction.net/", btns[0]["onclick"][15:][:-1])
+
+
+def sb_btn(tag):
+	try:
+		assert ["pageNav-jump", "pageNav-jump--next"] == tag["class"]
+		assert tag.text == "Next"
+		assert tag.name == "a"
+		return True
+	except (KeyError, AssertionError) as e:
+		return False
+
+def get_sb(url):
+	page = requests.get(url)
+	soup = bs4.BeautifulSoup(page.text, "html.parser")
+	btns = soup.find_all(sb_btn)
+
+	try:
+		assert btns[0].attrs["href"] == btns[1].attrs["href"]
+	except IndexError:
+		return None
+
+	return urljoin("https://forums.spacebattles.com/", btns[0].attrs["href"])
+
+def get_sv(url):
+	page = requests.get(url)
+	soup = bs4.BeautifulSoup(page.text, "html.parser")
+	btns = soup.find_all(sb_btn)
+
+	try:
+		assert btns[0].attrs["href"] == btns[1].attrs["href"]
+	except IndexError:
+		return None
+
+	return urljoin("https://forums.sufficientvelocity.com/", btns[0].attrs["href"])
+
+
+def qq_btn(tag):
+	try:
+		assert tag.name == "a"
+		assert ["text"] == tag["class"]
+		assert tag.text == "Next >"
+		return True
+	except (KeyError, AssertionError) as e:
+		return False
+
+
+def get_qq(url):
+	page = requests.get(url)
+	soup = bs4.BeautifulSoup(page.text, "html.parser")
+	btns = soup.find_all(qq_btn)
+
+	try:
+		assert btns[0].attrs["href"] == btns[1].attrs["href"]
+	except IndexError:
+		return None
+
+	return urljoin("https://forum.questionablequesting.com/", btns[0].attrs["href"])
+
+
+def check_nh(tag):
+	try:
+		assert tag.text == "404 – Not Found"
+		assert tag.name == "h1"
+		return True
+	except AssertionError as e:
+		return False
+
+
+def get_nh(url):
+	page = requests.get(url)
+	soup = bs4.BeautifulSoup(page.text, "html.parser")
+	if soup.find_all(check_nh):
+		return None
+
+	id_len = len(url[22:].split("/")[0])
+
+	if url[22 + id_len + 1:].count("/") > 0:
+		parts = url.split("/")
+		parts[-2] = str(int(parts[-2]) + 1)
+		new_url = "/".join(parts)
+		return new_url
+	else:
+		return url + "1/"
+
+
+
+def add_link(url):
+	while url:
+		delay = 50
+		errors = 0
+		while True:
+			try:
+				save.capture(url, user_agent="mr.awesome10000@gmail.com using savepagenow")
+				print(f"Saved: {url}")
+				time.sleep(30)
+				delay = 50
+				break
+			except Exception as e:
+				errors += 1
+				print(f"Error{errors}: {url}, {type(e)}: {e}")
+				delay += 650
+				time.sleep(delay)
+			
+
+
+		if url.startswith("https://www.fanfiction.net/"):
+			url = get_ffn(url)
+		elif url.startswith("https://forums.spacebattles.com/"):
+			url = get_sb(url)
+		elif url.startswith("https://forums.sufficientvelocity.com/"):
+			url = get_sv(url)
+		elif url.startswith("https://forum.questionablequesting.com/"):
+			url = get_qq(url)
+		elif url.startswith("https://nhentai.net/g/"):
+			url = get_nh(url)
+		else:
+			url = None
+
+
+if __name__ == "__main__":
+	#time.sleep(1)
+	
+	if "-f" in sys.argv:
+		with open("url_list.txt", "r") as file:
+			for url in file.readlines():
+				add_link(url.strip())
+	else:
+		for url in sys.argv[1:]:
+			add_link(url)
+
+
