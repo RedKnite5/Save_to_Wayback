aca062b5cd2a8bbd0c7778cb87e8dd821a151386
Max Friedman <mr.awesome10000@gmail.com>
2023-04-22 18:51:36 -0700

    improved checking if already saved

--- savetowayback.py
+++ savetowayback.py
@@ -16,10 +16,22 @@ import savepagenow as save
 
 
 NEW_URLS = "new_urls.txt"
+SAVED_URLS = "saved.txt"
 default_delay = 30
 toomanyrequests_delay = 60 * 5 + 30
 blocked_by_robots_delay = 120
 SAVE = True
+
+# TODO: Royal Road
+
+ff_url  = "https://www.fanfiction.net/"
+sb_url  = "https://forums.spacebattles.com/"
+sv_url  = "https://forums.sufficientvelocity.com/"
+qq_url  = "https://forum.questionablequesting.com/"
+nh_url  = "https://nhentai.net/g/"
+imh_url = "https://imhentai.xxx/"
+ao3_url = "https://archiveofourown.org/"
+
 
 # ffn now has NOARCHIVE and doesnt work
 
@@ -47,7 +59,7 @@ logger = setup_logger("first_logger", log_file)
 
 
 def prep_ao3_url(url):
-	if not url.startswith("https://archiveofourown.org/"):
+	if not url.startswith(ao3_url):
 		raise ValueError(f"Not AO3 url: {url}")
 
 	if url.endswith("#workskin"):
@@ -74,9 +86,9 @@ def get_ao3(url):
 	page = requests.get(url)
 	soup = bs4.BeautifulSoup(page.text, "html.parser")
 	btns = soup.find_all(ao3_btn)
-	
+
 	if btns:
-		next_url = urljoin("https://archiveofourown.org/", btns[0].attrs["href"])
+		next_url = urljoin(ao3_url, btns[0].attrs["href"])
 		return prep_ao3_url(next_url)
 	return None
 	
@@ -99,7 +111,7 @@ def get_ffn(url):
 		return None
 	assert btns[0]["onclick"].startswith("self.location='")
 
-	return urljoin("https://www.fanfiction.net/", btns[0]["onclick"][15:][:-1])
+	return urljoin(ff_url, btns[0]["onclick"][15:][:-1])
 
 
 def sb_btn(tag):
@@ -121,7 +133,7 @@ def get_sb(url):
 	except IndexError:
 		return None
 
-	return urljoin("https://forums.spacebattles.com/", btns[0].attrs["href"])
+	return urljoin(sb_url, btns[0].attrs["href"])
 
 def get_sv(url):
 	page = requests.get(url)
@@ -133,7 +145,7 @@ def get_sv(url):
 	except IndexError:
 		return None
 
-	return urljoin("https://forums.sufficientvelocity.com/", btns[0].attrs["href"])
+	return urljoin(sv_url, btns[0].attrs["href"])
 
 
 def qq_btn(tag):
@@ -155,7 +167,7 @@ def get_qq(url):
 	except IndexError:
 		return None
 
-	return urljoin("https://forum.questionablequesting.com/", btns[0].attrs["href"])
+	return urljoin(qq_url, btns[0].attrs["href"])
 
 
 def check_nh(tag):
@@ -175,7 +187,7 @@ def get_nh(url):
 		new_url = "/".join(parts)
 	else:
 		new_url = url + "1/"
-	
+
 	page = requests.get(new_url)
 	soup = bs4.BeautifulSoup(page.text, "html.parser")
 	if soup.find_all(check_nh):
@@ -189,7 +201,7 @@ def make_imh_checker(pages):
 			assert tag.name == "span"
 			assert tag["class"] == ["current"]
 			assert int(tag.text) > pages
-			
+
 			return True
 		except (AssertionError, KeyError):
 			return False
@@ -198,7 +210,7 @@ def make_imh_checker(pages):
 def total_pages_imh(tag):
 	try:
 		assert tag.name == "span"
-		
+
 		logger.debug(f"{tag['class'] = }")
 		assert tag["class"] == ["total_pages"]
 		return True
@@ -207,7 +219,7 @@ def total_pages_imh(tag):
 
 def get_imh(url):
 	logger.debug(f"Getting next imh style url from {url}")
-	
+
 	new_url = None
 
 	if not url.endswith("/"):
@@ -219,10 +231,10 @@ def get_imh(url):
 		parts = url.split("/")
 		parts[-2] = str(int(parts[-2]) + 1)
 		new_url = "/".join(parts)
-	
+
 	page = requests.get(new_url)
 	soup = bs4.BeautifulSoup(page.text, "html.parser")
-	
+
 	total_page_tag = soup.find(total_pages_imh)
 	if total_page_tag is None:
 		# should not write to saved file in this case
@@ -231,9 +243,9 @@ def get_imh(url):
 		logger.error(f"new redirecting url: {new_url}")
 		return None
 	pages = int(total_page_tag.text)
-	
+
 	logger.debug(f"imh total page count is {pages}")
-	
+
 	if soup.find_all(make_imh_checker(pages)):
 		return None
 	return new_url
@@ -241,13 +253,13 @@ def get_imh(url):
 
 def add_link(url_original):
 	last_url = None
-	
+
 	url = url_original.strip()
-	if url.startswith("https://archiveofourown.org/"):
+	if url.startswith(ao3_url):
 		url = prep_ao3_url(url)
 
 	while url:
-		
+
 		errors = 0
 		while True:
 			try:
@@ -259,7 +271,7 @@ def add_link(url_original):
 
 				time.sleep(default_delay)
 				print(f"Saved: {url}")
-				
+
 				logging.info(f"Saved: {url}")
 				break
 			except save.BlockedByRobots as e:
@@ -279,19 +291,19 @@ def add_link(url_original):
 		else:
 			last_url = url_original.strip()
 
-		if url.startswith("https://www.fanfiction.net/"):
+		if url.startswith(ff_url):
 			url = get_ffn(url)
-		elif url.startswith("https://forums.spacebattles.com/"):
+		elif url.startswith(sb_url):
 			url = get_sb(url)
-		elif url.startswith("https://forums.sufficientvelocity.com/"):
+		elif url.startswith(sv_url):
 			url = get_sv(url)
-		elif url.startswith("https://forum.questionablequesting.com/"):
+		elif url.startswith(qq_url):
 			url = get_qq(url)
-		elif url.startswith("https://nhentai.net/g/"):
+		elif url.startswith(nh_url):
 			url = get_nh(url)
-		elif url.startswith("https://imhentai.xxx/"):
+		elif url.startswith(imh_url):
 			url = get_imh(url)
-		elif url.startswith("https://archiveofourown.org/"):
+		elif url.startswith(ao3_url):
 			url = get_ao3(url)
 		else:
 			url = None
@@ -299,9 +311,9 @@ def add_link(url_original):
 	return last_url
 
 
-def read_saved():
+def read_saved() -> list[str]:
 	if not hasattr(read_saved, "lines"):
-		with open("saved.txt", "r") as save_file:
+		with open(SAVED_URLS, "r") as save_file:
 			lines = list(line.strip() for line in save_file.readlines())
 		read_saved.lines = lines
 	return read_saved.lines
@@ -321,27 +333,47 @@ def accumulator_factory_startswith(url):
 
 def is_updatatable(url):
 	"Or all possible failure conditions then invert the result"
-	
+
 	not_updatable = [
-		"https://nhentai.net/g/",
-		"https://imhentai.xxx/"
+		nh_url,
+		imh_url
 	]
-	
+
 	return not reduce(accumulator_factory_startswith(url), not_updatable)
 
 def is_saved(url):
 	lines = read_saved()
-	if url in lines:
+	formatted = save_format(url)
+	if url in lines or formatted in lines:
 		return True
 	
-	cutoff_page = [line.rsplit("/", 1)[0] for line in lines]
-	if url in cutoff_page:
-		return True
-	if url.rsplit("/", 1)[0] in cutoff_page:
-		return True
+	# TODO: caching
+	cutoff_page = []
+	for line in lines:
+		if line.startswith(sb_url) or line.startswith(sv_url) or line.startswith(qq_url):
+			cutoff_page.append(line.split("/page-")[0])
+		elif line.startswith(nh_url):
+			cutoff_page.append(nh_url + "g/" + line[len(nh_url + "g/"):].split("/")[0])
+		elif line.startswith(imh_url):
+			new_line = line.strip("/")
+			if "view" in url:
+				new_line = new_line.replace("view", "gallery")
+				new_line = new_line.rsplit("/")[0]
+			cutoff_page.append(new_line)
+		else:  # TODO: ao3  
+			cutoff_page.append(line.strip("/"))
 	
+	if formatted in cutoff_page:
+		return True
+
+	#cutoff_page = [line.rsplit("/", 1)[0] for line in lines]
+	#if url in cutoff_page:
+	#	return True
+	#if url.rsplit("/", 1)[0] in cutoff_page:
+	#	return True
+
 	return False
-	
+
 
 def update_old(lines):
 	for index, preurl in enumerate(list(lines)):
@@ -352,21 +384,32 @@ def update_old(lines):
 				lines[index] = last
 
 
-
 def save_url_list(urls, lines, url_queue):
 	url_queue.extend(url.strip() for url in urls)
 	for url in urls:
 		if is_saved(url):
 			continue
-		last = add_link(url)
+		last = save_format(add_link(url))
 		if last:
 			lines.append(last)
 			logger.debug(f"appending {last} to lines")
 		url_queue.popleft()
 		if SAVE:
-			write_saved(lines, "saved.txt")
+			write_saved(lines, SAVED_URLS)
 			write_saved(url_queue, NEW_URLS)
 			logger.info("Saving")
+
+
+def save_format(url):
+	"""Format the url to be saved in the save file"""
+	url = url.strip("/")
+	if url.startswith(imh_url):
+		if "view" in url:
+			url = url.replace("view", "gallery")
+			url = url.rsplit("/")[0]
+		return url
+	return url
+	
 
 
 
@@ -382,29 +425,27 @@ HELP = """Usage: python3 savetowayback.py [-uf] [URLS]...
 
 def main():
 	lines = read_saved()
-	
+
 	if "--help" in sys.argv:
 		print(HELP)
 		return
 
-	
 	try:
 		given = sys.argv[1:]
 		todo_file = NEW_URLS
-		save_file = "saved.txt"
-		
-		
+		save_file = SAVED_URLS
+
 		if "-u" in sys.argv:
 			update_old(lines)
 			given.remove("-u")
-		
+
 		url_queue = deque()
 		if "-f" in sys.argv:
 			given.remove("-f")
 			with open(NEW_URLS, "r") as file:
 				urls = file.readlines()
 			save_url_list(urls, lines, url_queue)
-		
+
 		save_url_list(given, lines, url_queue)
 
 		lines = list(set(lines))
@@ -415,9 +456,9 @@ def main():
 			todo_file = "urls_unfinished.txt"
 			save_file = "save_dump.txt"
 			log_message = "Uncaught Fatal Exception"
-		
+
 		logger.critical(log_message)
-		
+
 		raise
 	finally:
 		logger.info("Stopping")
