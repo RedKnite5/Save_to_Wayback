a0a92c66c170997740cc8dd6c2c3208e07a59443
Max <mr.awesome10000@gmail.com>
2024-06-13 16:38:55 -0700

    move formatters into main file

--- savetowayback.py
+++ savetowayback.py
@@ -113,16 +113,96 @@ class FlexibleLogger(logging.Logger):
 
 		super()._log(level, msg, args, exc_info, extra, stack_info, stacklevel)
 
-def setup_logging() -> logging.Logger:
-	with open_utf8("logging_config.json") as file:
-		config = json.load(file)
-	logging.config.dictConfig(config)
+def dict_replace(string: str, dictionary: Mapping[str, str]) -> str:
+	for find, replace in dictionary.items():
+		string = string.replace(find, replace)
+	return string
 
-	logging.setLoggerClass(FlexibleLogger)
+class STDOutFormatter(logging.Formatter):
+	abvreviations = {
+		FF_URL: "FF/",
+		f"{SB_URL}threads/": "SB/",
+		f"{SV_URL}/threads/": "SV/",
+		f"{QQ_URL}threads/": "QQ/",
+		NH_URL: "NH/",
+		IMH_URL: "IMH/",
+		f"{AO3_URL}works/": "AO3/",
+		f"{RR_URL}fiction/": "RR/",
+	}
 
-	return logging.getLogger("savetowayback")
+	def __init__(self, *, datefmt: str | None = None):
+		super().__init__(fmt="%(asctime)s %(exc_name)s%(message)s", datefmt=datefmt)
 
-logger = setup_logging()
+	def format(self, record: logging.LogRecord) -> str:
+		url = getattr(record, "url", None)
+		url = str(url)
+		url = dict_replace(url, self.abvreviations)
+
+		record.message = record.getMessage()
+		record.message = record.message.format(url=url)
+
+		exc_name = ""
+		if record.exc_info:
+			exc_name = self.formatException(record.exc_info)
+		record.exc_name = exc_name
+
+		if self.usesTime():
+			record.asctime = self.formatTime(record, self.datefmt)
+		message = self.formatMessage(record)
+
+		return message
+
+	def formatException(self, ei: ExceptionInfo) -> str:
+		if isinstance(ei, tuple):
+			if ei[0] is not None:
+				exc_name = ei[0].__name__
+			else:
+				exc_name = "None"
+		else:
+			if isinstance(ei, BaseException):
+				exc_name = type(ei).__name__
+			else:
+				exc_name = "None"
+		return exc_name + " "
+
+class FileFormatter(logging.Formatter):
+	def __init__(self, *, datefmt: str | None = None):
+		super().__init__(fmt="%(asctime)s %(message)s", datefmt=datefmt)
+
+	def format(self, record: logging.LogRecord) -> str:
+		url = getattr(record, "url", None)
+		url = str(url)
+
+		record.message = record.getMessage()
+		record.message = record.message.format(url=url)
+
+		if self.usesTime():
+			record.asctime = self.formatTime(record, self.datefmt)
+		message = self.formatMessage(record)
+
+		if record.exc_info:
+			message += self.formatException(record.exc_info)
+
+		return message
+
+	def formatException(self, ei: ExceptionInfo) -> str:
+		if isinstance(ei, tuple):
+			if ei[0] is not None:
+				exc_name = ei[0].__name__
+				exc_args = str(ei[1])
+			else:
+				exc_name = "None"
+				exc_args = "Empty"
+		else:
+			if isinstance(ei, BaseException):
+				exc_name = type(ei).__name__
+				exc_args = str(ei)
+			else:
+				exc_name = "None"
+				exc_args = "Empty"
+
+		return f"\n{exc_name}: {exc_args}"
+
 
 def cut_end(string: str, ending: str) -> str:
 	if string.endswith(ending):
@@ -740,6 +820,15 @@ def parse_args_and_save(saved: Saved) -> None:
 
 	save_url_list(given, saved, save_to_new=False)
 
+def setup_logging() -> logging.Logger:
+	with open_utf8("logging_config.json") as file:
+		config = json.load(file)
+	logging.config.dictConfig(config)
+
+	logging.setLoggerClass(FlexibleLogger)
+
+	return logging.getLogger("savetowayback")
+
 def main() -> None:
 	saved = Saved(SAVED_URLS)
 
@@ -761,5 +850,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+	logger = setup_logging()
 	logger.info("Starting")
 	main()
